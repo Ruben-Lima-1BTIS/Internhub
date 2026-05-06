8d4e824a10e3dd25d89d5cd66c8b2c7092b5069a
Ruben <5488@eclisboa.net>
2026-05-06 18:30:04 +0100

    fix: ensure role is set for Admin user in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@gmail.com',
+            'role' => 'admin',
             'password' => bcrypt('Admin123!'),
         ]);
     }
